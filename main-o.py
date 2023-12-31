5632a658da4daa43aa901d0f947a98187de27f21
jakelime <jiahao89sg@gmail.com>
2023-12-31 10:23:04 +0800

    init o-auth app

---
+++ main-o.py
@@ -0,0 +1,83 @@
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+import base64
+from email import message_from_bytes
+from google.oauth2.credentials import Credentials
+
+# If modifying these SCOPES, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def get_gmail_service():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # Build the Gmail API service
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def get_messages(service, user_id='me', label_ids=['INBOX']):
+    try:
+        # Get a list of messages
+        response = service.users().messages().list(userId=user_id, labelIds=label_ids).execute()
+        messages = response.get('messages', [])
+
+        # Print the subject and sender of each message
+        for message in messages:
+            msg = service.users().messages().get(userId=user_id, id=message['id']).execute()
+            headers = msg['payload']['headers']
+            subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject')
+            sender = next((header['value'] for header in headers if header['name'] == 'From'), 'No Sender')
+            print(f"Subject: {subject}")
+            print(f"From: {sender}")
+            print()
+
+    except Exception as error:
+        print(f"An error occurred: {error}")
+
+if __name__ == '__main__':
+    import os.path
+    creds = None
+
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    # Build the Gmail API service
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Get and print the messages in the user's inbox
+    get_messages(service)
